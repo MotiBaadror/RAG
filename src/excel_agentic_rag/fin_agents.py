a4a6c39950f22ef7b60006684a78f0d40b4addca
motiram <moti@playai.network>
2024-11-29 11:37:57 +0530

    using llama3.1

--- src/excel_agentic_rag/fin_agents.py
+++ src/excel_agentic_rag/fin_agents.py
@@ -1,21 +1,48 @@
 from llama_index.core.agent import ReActAgent
 from llama_index.core.tools import FunctionTool
+from pydantic import Field
 
 
-def shareholder_equity_fn(share_price:int,total_outstanding_shares):
-    "Calculate the shareholder equity, given share_price and total_outstanding share "
+def calculate_shareholder_equity(
+        share_price: float = Field(
+        description="Share price of the company <value>"
+        ),
+        total_outstanding_shares: int = Field(
+        description="Total outstanding shares of the company price of the company <value>"
+    )
+    ) -> float:
+
+    """return the shareholder equity, given share_price and total_outstanding share, this is same
+     for company market capitalization"""
     return share_price*total_outstanding_shares
+
+
+def calculate_eps(
+        net_income: float= Field(
+            description="net company income formated like <value> "
+        ),
+        outstanding_shares: int = Field(
+            description="outstanding shares of the company formated like <value>"
+        )
+    ) -> float:
+    """return EPS for given companies net income and total outstanding shares"""
+    return net_income/outstanding_shares
+
 
 def get_agents_from_fn(llm, fns):
     tools = [FunctionTool.from_defaults(fn=fn) for fn in fns]
     agent = ReActAgent.from_tools(tools, llm=llm, verbose=True)
     return agent
 
+
 def get_tools_from_fns(fns):
     tools = [FunctionTool.from_defaults(fn=fn) for fn in fns]
     return tools
 
-def get_agent_from_tools(llm, tools):
-    agent = ReActAgent.from_tools(tools, llm=llm, verbose=True)
+
+def get_agent_from_tools(llm, tools, context=None,max_iterations=10):
+    if context is None:
+        print('No context is provided for tool')
+    agent = ReActAgent.from_tools(tools, llm=llm, verbose=True, max_iterations=max_iterations)
     return agent
 
--- src/excel_agentic_rag/rag_agent_improved_prompts.py
+++ src/excel_agentic_rag/rag_agent_improved_prompts.py
@@ -6,7 +6,7 @@ from excel_agentic_rag.fin_agents import get_agents_from_fn, get_tools_from_fns,
 from learn_llama_index.set_index import get_index
 from learn_llama_index.set_llm import set_llm
 
-model = 'llama3.1'
+model = 'llama3.2'
 set_llm(model=model)
 
 index = get_index(f"storage/codelon_data_{model}", "data/codelon_data", use_llamaparse=True)
@@ -34,14 +34,17 @@ finance_tool = QueryEngineTool(
     )
 )
 all_tools = func_tools+[finance_tool]
+context = """ Purpose: 
+The primary role of this agent is to retrieve data from a company data tool in response to user queries and do any calculation
+using other defined tool as per the query requirements."""
 
-agent = get_agent_from_tools(llm=Settings.llm, tools=all_tools,max_iterations=50)
+agent = get_agent_from_tools(llm=Settings.llm, tools=all_tools,context=context)
 # prompt = "Which of the companies is potentially overvalued?"
 # prompt = "Give me shareholder equity for FinServ company"
 # prompt = "Give me share price for FinServ company, Go step by step, using a tool to do any math."
 prompt = "What would the EPS be for Techcorp if there is a 3 for 1 stock split?"
 # final_prompt = f"{prompt}, use company tool to retrieve data and other tool for calculation"
-final_prompt = f"{prompt},Use company_data tool to get all required values for the computation, Go step by step, using a tool to do any math."
+final_prompt = f"{prompt}"
 
 
 
