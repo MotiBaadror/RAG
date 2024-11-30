d326d4d655e7e390ae63e2c72e4b19593f1a01be
motiram <moti@playai.network>
2024-11-30 12:18:29 +0530

    cleaning

--- tests/test_set_index.py
+++ tests/test_set_index.py
@@ -5,4 +5,4 @@ def test_get_files():
     data_path = 'data/codelon_data'
     results = get_files(data_path=data_path)
     print(results)
-    assert len(results) ==1
+    assert len(results) == 1
